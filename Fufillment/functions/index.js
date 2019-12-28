7a2ee99b3d1c12fc9c9d3aadf79a0b8bb37809b9
Michael barney Junior <michaelbarneyjunior@gmail.com>
2019-12-28 13:40:43 -0300

    added enviromente variables

--- Fufillment/functions/index.js
+++ Fufillment/functions/index.js
@@ -1,34 +1,78 @@
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
+/**
+ * Firebase Function do Casper Chatbot
+ * Michael Barney - 2019
+ */
+
 'use strict';
  
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
- 
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://elife:elife@cluster0-rojfb.gcp.mongodb.net/test?retryWrites=true&w=majority";
-
 var fetch = require("node-fetch");
-
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = 'dialogflow:debug';
  
-// Dialogflow Webhook
+/**
+ *  Firebase Funcion
+ *  Chamada após receber um Request do Dialogflow na URL específicada.
+ *  Retorna uma resposta ao Agente do Dialogflow.
+*/
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+
+    // Analisar a mensagem recebida e delegar a função correta à ser executada de acordo com seu Intent.
     const agent = new WebhookClient({ request, response });
-
-
     let intentMap = new Map();
     intentMap.set('News Intent', getNews);
     agent.handleRequest(intentMap);
 
+    /**
+     * Função para o intent getNews.
+     * Retorna um carrousel de notícias.
+     * @param {Dialogflow Agent} agent 
+     */
+    async function getNews(agent) {
+        // Receber o tema da notícia
+        const theme = agent.parameters.theme;
+        agent.add("Estas são as notícias sobre " + theme + " que encontrei:");
 
+        // Requisitar notícias do Banco de Dados
+        const newsGroup = await getNewsFromDB(theme);
+
+        // Montar o Carrousel de Notícias
+        if (newsGroup.length > 0 ){
+            for (let news of newsGroup){
+                agent.add(new Card({
+                    title: news.title,
+                    imageUrl: news.imageUrl,
+                    text: news.description,
+                    buttonText: 'Saiba Mais', 
+                    buttonUrl: news.link
+                    })
+                );
+            }
+        }
+        else{
+            // Nenhuma notícia foi encontrada
+            agent.setFollowupEvent("no_news_found");
+        }
+
+        // Montar o menu de temas de notícias
+        const quickReplies = new Suggestion({
+            title: "Você quer ver mais notícias?",
+            reply: "⚽ Esportes"
+        })
+        quickReplies.addReply_("⚖️ Política")
+        quickReplies.addReply_("🎥 Entretenimento")
+        quickReplies.addReply_("🤩 Famosos")
+        agent.add(quickReplies);
+    }
+
+    /**
+     * Pesquisa no Banco de Dados as notícias de dado tema.
+     * @param {string} theme - O tema das notícias à serem pesquisadas
+     */
     async function getNewsFromDB(theme){
-        const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+        const client = await MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .catch(err => { console.log(err); });
 
         if (!client) {
@@ -40,7 +84,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             let collection = db.collection('news');
             let query = { theme: theme }
             let res = await collection.find(query).toArray();
-            console.log(res);
             return res;
         } catch (err) {
             console.log(err);
@@ -48,52 +91,18 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             client.close();
         }
     }
-
-    async function getNews(agent) {
-    const theme = agent.parameters.theme;
-
-    agent.add("Estas são as notícias sobre " + theme + " que encontrei:");
-
-    // Request data from MongoDB
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    console.log("a: " + theme);
-    const newsGroup = await getNewsFromDB(theme);
-    console.log("b");
-
-    if (newsGroup.length > 0 ){
-        for (let news of newsGroup){
-            agent.add(new Card({
-                title: news.title,
-                imageUrl: news.imageUrl,
-                text: news.description,
-                buttonText: 'Saiba Mais', 
-                buttonUrl: news.link
-                })
-            );
-        }
-    }
-    else{
-        agent.setFollowupEvent("no_news_found");
-    }
-
-    const quickReplies = new Suggestion({
-        title: "Você quer ver mais notícias?",
-        reply: "⚽ Esportes"
-    })
-    quickReplies.addReply_("⚖️ Política")
-    quickReplies.addReply_("🎥 Entretenimento")
-    quickReplies.addReply_("🤩 Famosos")
-
-    agent.add(quickReplies);
-
-    }
 });
 
-// News Updater
+/**
+ *  Firebase Funcion
+ *  Chamada diariamente às 00:01
+ *  Preenche o banco de dados com notícias coletadas de uma API extrarna. 
+*/
 exports.newsUpdate = functions.pubsub.schedule('1 0 * * *')
     .timeZone('America/Sao_Paulo')
-    .onRun(async (context) => {  
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+    .onRun(async () => {  
+
+    const client = await MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .catch(err => { console.log(err); });
 
     if (!client) {
@@ -102,12 +111,14 @@ exports.newsUpdate = functions.pubsub.schedule('1 0 * * *')
     try {
         const db = client.db("casper");
         let collection = db.collection('news');
-        //get all news
+
+        //Coletar notícias
         let allNews = await getAllNews()
 
-        // clear the collection
+        //Limpar a coleção
         collection.deleteMany({})
-        console.log(allNews);
+
+        // Preencher as notícias
         collection.insertMany(allNews)
 
     } catch (err) {
@@ -116,23 +127,25 @@ exports.newsUpdate = functions.pubsub.schedule('1 0 * * *')
         client.close();
     }
 
+    /**
+     * Coleta 10 notícias de cada tema através da NewsAPI
+     * Retorna um array com objetos representando as notícias.
+     */
     async function getAllNews(){
         const themes = ["esportes", "entretenimento", "famosos", "política"];
 
         var result = [];
 
         for (let theme of themes){
-            console.log("******"+theme+"**********")
             const news = await getNews(theme)
             for (let i in  news.articles){
                 if(i < 10){
                     let article = news.articles[i];
-                    console.log("title: " + article.title)
                     result.push({
                         title: article.title,
                         imageUrl: article.urlToImage,
                         description: article.description,
-                        link: await getURL(article.url),
+                        link: await minifyURL(article.url),
                         theme: theme
                     })
                 }
@@ -145,8 +158,30 @@ exports.newsUpdate = functions.pubsub.schedule('1 0 * * *')
         return result;
     }
 
-    async function getURL(longURL){
-        let queryURL = "https://api.rebrandly.com/v1/links/new?destination="+longURL+"&apikey=c95033066865402eb6d1dc40a4c4547f";
+    /**
+     * Requere à NewsAPI notícias em português do tema específicado.
+     * Retorna um objeto com as informações da notícia.
+     * @param {String} theme  - O tema da notícia à ser pesquisada.
+     */
+    async function getNews(theme){
+        var url = "https://newsapi.org/v2/everything?q="+theme.replace("í","i")+"&sortBy=publishedAt&language=pt&apiKey=" + process.env.NEWS_API_KEY;
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            return {};
+        }
+    }
+
+    /**
+     * Minifica urls através da API da Rebrangly
+     * Returna o url minificado.
+     * @param {String} longURL 
+     * @returns {String}
+     */
+    async function minifyURL(longURL){
+        let queryURL = "https://api.rebrandly.com/v1/links/new?destination="+longURL+"&apikey="+process.env.REBRANDLY_API_KEY;
         try {
             const response = await fetch(queryURL);
             const json = await response.json();
@@ -154,19 +189,6 @@ exports.newsUpdate = functions.pubsub.schedule('1 0 * * *')
         } catch (error) {
             console.log(error)
             return "";
-        }
-    }
-
-    async function getNews(theme){
-        // Get news from API
-        var url = "https://newsapi.org/v2/everything?q="+theme.replace("í","i")+"&sortBy=publishedAt&language=pt&apiKey=c94f6bc4f1154d60b02095acecf75815";
-        console.log("url: " + url);
-        try {
-            const response = await fetch(url);
-            const json = await response.json();
-            return json;
-        } catch (error) {
-            return {};
         }
     }
 
